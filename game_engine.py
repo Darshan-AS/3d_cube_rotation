8ddf9c33f5d20fdae3edf61dff39ea196729636c
Darshan A S <itisdarshan@gmail.com>
2020-01-20 17:45:24 +0530

    Add feature to rotate using mouse drag

--- game_engine.py
+++ game_engine.py
@@ -1,8 +1,10 @@
+import math
 import sys
 
 import pygame
 
 from cube import Cube
+from point import Point2D
 
 
 class GameEngine:
@@ -15,11 +17,28 @@ class GameEngine:
 
     def run(self):
         angle_x, angle_y, angle_z = 0.0, 0.0, 0.0
+        delta_x, delta_y, delta_z = 0, 0, 0
 
+        width, height = self.screen.get_width(), self.screen.get_height()
+        mouse_start, mouse_end = None, None
+        drag = False
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    drag = True
+                    mouse_start = Point2D(*event.pos)
+                    print(mouse_start)
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    drag = False
+                elif event.type == pygame.MOUSEMOTION and drag:
+                    curr_pos = Point2D(*event.pos)
+                    delta_y = (mouse_start.x - curr_pos.x) * 2 * math.pi / width
+                    delta_x = (mouse_start.y - curr_pos.y) * 2 * math.pi / height
+
+                    angle_x += delta_x
+                    angle_y += delta_y
 
             self.clock.tick(50)
             self.screen.fill((0, 0, 0))
@@ -38,9 +57,9 @@ class GameEngine:
                 pygame.draw.line(self.screen, (255, 255, 255), vertices_2d[2], vertices_2d[3])
                 pygame.draw.line(self.screen, (255, 255, 255), vertices_2d[3], vertices_2d[0])
 
-            # angle_x += 1
-            angle_y += 1
-            # angle_z += 1
+            angle_x += delta_x
+            angle_y += delta_y
+            angle_z += delta_z
 
             pygame.display.flip()
 
